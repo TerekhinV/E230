9ef3fcde06911b021f17b1daf4db7dffab393512
Vlad Terekhin <C0503610@online.camosun.ca>
2023-09-07 10:05:59 -0700

    Basic functional calculator (Lab2)

---
+++ Lab2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float a, b, c;
+        do{
+            Console.WriteLine("Enter a:");
+            a = (float)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            b = (float)Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter op (+|-|*|/):");
+            string op = Console.ReadLine();
+            Console.Write("Result: ");
+            switch (op){
+                case "+": Console.WriteLine(a+b); break;
+                case "-": Console.WriteLine(a-b); break;
+                case "*": Console.WriteLine(a*b); break;
+                case "/": Console.WriteLine(a/b); break;
+                default: Console.WriteLine("Invalid operator"); break;
+            }
+            Console.WriteLine("Exit? (y|n)");
+        } while (Console.ReadLine().Equals("n"));
+    }
+}
