4059d48eabbb553bfdc064cdf1347d1e04ecae79
Vlad Terekhin <C0503610@online.camosun.ca>
2023-09-07 08:40:04 -0700

    Account check on VSCode

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,5 +4,6 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("So turns out you don't need to be signed in as a specified account in order to use its email in the commit author entry");
+        Console.WriteLine("Also VSCode works fine (make sure you have the C# devtools installed)");
     }
 }
