ec651208dd26d04d9ce8d9700fc6a4f080feec7d
Vlad Terekhin <C0503610@online.camosun.ca>
2023-09-13 13:12:22 -0700

    Added power/root ops

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -3,14 +3,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        float? a, b;
+        double? a, b;
         bool loopFlag;
 
         do{//main loop
             do{//continuously grab input until it's valid
                 try{
                     Console.WriteLine("Enter a:");
-                    a = (float)Convert.ToDouble(Console.ReadLine());
+                    a = (double)Convert.ToDouble(Console.ReadLine());
                 } catch {
                     Console.Write("Invalid input; ");
                     a = null;
@@ -20,7 +20,7 @@ internal class Program
             do{//same for B
                 try{
                     Console.WriteLine("Enter b:");
-                    b = (float)Convert.ToDouble(Console.ReadLine());
+                    b = (double)Convert.ToDouble(Console.ReadLine());
                 } catch {
                     Console.Write("Invalid input; ");
                     b = null;
@@ -29,12 +29,14 @@ internal class Program
 
             do{//same-ish for operator (implemented differently)
                 loopFlag = false;
-                Console.WriteLine("Enter op (+|-|*|/):");
+                Console.WriteLine("Enter op (+|-|*|/|^|root):");
                 switch (Console.ReadLine() ?? ""){
                     case "+": Console.WriteLine($"Result: {a+b}"); break;
                     case "-": Console.WriteLine($"Result: {a-b}"); break;
                     case "*": Console.WriteLine($"Result: {a*b}"); break;
                     case "/": Console.WriteLine($"Result: {a/b}"); break;
+                    case "^": Console.WriteLine($"Result: {Math.Pow((double)a, (double)b)}"); break;
+                    case "root": Console.WriteLine($"Result: {Math.Pow((double)a, 1/(double)b)}"); break;
                     default: Console.Write("Invalid operator; "); loopFlag = true; break;
                 }
             } while (loopFlag);
