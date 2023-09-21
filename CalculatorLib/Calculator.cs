735de58a7c7269be501f2ee65b0370f1b3ab2545
Vlad Terekhin <C0503610@online.camosun.ca>
2023-09-21 12:14:52 -0700

    Basic GUI calculator. Breaks console calculator, removed logging features due to write location issues

--- CalculatorLib/Calculator.cs
+++ CalculatorLib/Calculator.cs
@@ -1,24 +1,9 @@
 ﻿namespace CalculatorLib;
 using System;
-using System.Diagnostics;
-using Newtonsoft.Json;
 public class Calculator{
-    JsonWriter writer;
     public Calculator(){
-        StreamWriter logFile = File.CreateText("calculatorlog.json");
-        logFile.AutoFlush = true;
-        writer = new JsonTextWriter(logFile);
-        writer.Formatting = Formatting.Indented;
-        writer.WriteStartObject();
-        writer.WritePropertyName("Operations");
-        writer.WriteStartArray();
     }
-    public void Finish(){
-        writer.WriteEndArray();
-        writer.WriteEndObject();
-        writer.Close();
-    }
-    public double doOp(double a, double b, string op){
+    public static double doOp(double a, double b, string op){
         double res;
         try{
             switch (op){
@@ -27,19 +12,9 @@ public class Calculator{
                 case "*":       res = a*b; break;
                 case "/":       res = a/b; break;
                 case "^":       res = Math.Pow(a, b); break;
-                case "root":    res = Math.Pow(a, 1/b); break;
+                case "√":    res = Math.Pow(b, 1/a); break;
                 default:        return double.NaN;
             }
-            writer.WriteStartObject();
-            writer.WritePropertyName("Operand1");
-            writer.WriteValue(a);
-            writer.WritePropertyName("Operand2");
-            writer.WriteValue(b);
-            writer.WritePropertyName("Operation");
-            writer.WriteValue(op);
-            writer.WritePropertyName("Result");
-            writer.WriteValue(res);
-            writer.WriteEndObject();
             return res;
         }catch{return double.NaN;}
     }
--- Lab3/MainPage.xaml.cs
+++ Lab3/MainPage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿namespace Lab3;
-
+using CalculatorLib;
 public partial class MainPage : ContentPage
 {
 	public MainPage()
@@ -7,7 +7,24 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnButtonClicked(object s, EventArgs e){
-		
-	}
+    private void OperatorClicked(object s, EventArgs e)
+    {
+        op.Text = (s as Button).Text;
+    }
+    private void Evaluate(object s, EventArgs e)
+    {
+        double a, b, o;
+        if (!double.TryParse(numA.Text, out a))
+        {
+            output.Text = "Failed to parse input A";
+            return;
+        }
+        if (!double.TryParse(numB.Text, out b))
+        {
+            output.Text = "Failed to parse input B";
+            return;
+        }
+        o = Calculator.doOp(a, b, op.Text);
+        output.Text = double.IsNaN(o) ? "Operation error" : o.ToString();
+    }
 }
