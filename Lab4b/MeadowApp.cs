2f236c05b4f0b673c2f79a30752c81cd40751748
Vlad Terekhin <C0503610@online.camosun.ca>
2023-10-19 13:03:14 -0700

    Bouncy balls

---
+++ Lab4b/MeadowApp.cs
@@ -0,0 +1,120 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Leds;
+using Meadow.Hardware;
+using Meadow.Units;
+using System;
+
+namespace Lab4b
+{
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    public class MeadowApp : App<F7FeatherV2>
+    {
+        readonly Color WatchBackgroundColor = Color.White;
+
+        MicroGraphics graphics;
+        int displayWidth, displayHeight;
+        int hour, minute, tick;
+
+        public MeadowApp()
+        {
+            Initialize();
+
+            DrawShapes();
+        }
+
+        void Initialize()
+        {
+            var onboardLed = new RgbPwmLed(
+                device: Device,
+                redPwmPin: Device.Pins.OnboardLedRed,
+                greenPwmPin: Device.Pins.OnboardLedGreen,
+                bluePwmPin: Device.Pins.OnboardLedBlue);
+            onboardLed.SetColor(Color.Red);
+
+            var config = new SpiClockConfiguration(
+                speed: new Frequency(48000, Frequency.UnitType.Kilohertz),
+                mode: SpiClockConfiguration.Mode.Mode3);
+            var spiBus = Device.CreateSpiBus(
+                clock: Device.Pins.SCK,
+                copi: Device.Pins.MOSI,
+                cipo: Device.Pins.MISO,
+                config: config);
+            var st7789 = new St7789
+            (
+                device: Device,
+                spiBus: spiBus,
+                chipSelectPin: Device.Pins.D02,
+                dcPin: Device.Pins.D01,
+                resetPin: Device.Pins.D00,
+                width: 240, height: 240
+            );
+            displayWidth = Convert.ToInt32(st7789.Width);
+            displayHeight = Convert.ToInt32(st7789.Height);
+
+            graphics = new MicroGraphics(st7789);
+            graphics.Rotation = RotationType._270Degrees;
+
+            onboardLed.SetColor(Color.Green);
+        }
+
+        void DrawShapes()
+        {
+            Random rand = new Random();
+
+            graphics.Clear(true);
+
+            int radius = 10;
+            int originX = displayWidth / 2;
+            int originY = displayHeight / 2;
+
+            for (int i = 1; i < 5; i++)
+            {
+                graphics.DrawCircle
+                (
+                    centerX: originX,
+                    centerY: originY,
+                    radius: radius,
+                    color: Color.FromRgb(
+                        rand.Next(128, 255), rand.Next(128, 255), rand.Next(128, 255))
+                );
+                graphics.Show();
+                radius += 30;
+            }
+
+            int sideLength = 30;
+            for (int i = 1; i < 5; i++)
+            {
+                graphics.DrawRectangle
+                (
+                    x: (displayWidth - sideLength) / 2,
+                    y: (displayHeight - sideLength) / 2,
+                    width: sideLength,
+                    height: sideLength,
+                    color: Color.FromRgb(
+                        rand.Next(128, 255), rand.Next(128, 255), rand.Next(128, 255))
+                );
+                graphics.Show();
+                sideLength += 60;
+            }
+
+            graphics.DrawLine(0, displayHeight / 2, displayWidth, displayHeight / 2,
+                Color.FromRgb(
+                    rand.Next(128, 255), rand.Next(128, 255), rand.Next(128, 255)));
+            graphics.DrawLine(displayWidth / 2, 0, displayWidth / 2, displayHeight,
+                Color.FromRgb(
+                    rand.Next(128, 255), rand.Next(128, 255), rand.Next(128, 255)));
+            graphics.DrawLine(0, 0, displayWidth, displayHeight,
+                Color.FromRgb(
+                    rand.Next(128, 255), rand.Next(128, 255), rand.Next(128, 255)));
+            graphics.DrawLine(0, displayHeight, displayWidth, 0,
+                Color.FromRgb(
+                    rand.Next(128, 255), rand.Next(128, 255), rand.Next(128, 255)));
+            graphics.Show();
+
+            //Thread.Sleep(5000);
+        }
+    }
+}
---
+++ Lab6/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System.Timers;
+
+namespace Lab6
+{
+    public partial class MainPage : ContentPage
+    {
+        static System.Timers.Timer timer; //time control
+        static Ball[] balls; //main array
+        static int limit = 1000; //number of balls
+        static double frameTarget = 60; //FPS target
+        static int mspf = (int)(1000 / frameTarget); //to milliseconds
+        static double multiplier = 1; //speed
+        static double compmultiplier = multiplier * mspf * frameTarget / 1000; //adjust speed to FPS
+        public MainPage()
+        {
+            InitializeComponent();
+            timer = new System.Timers.Timer();
+            timer.Interval = mspf;
+            timer.Elapsed += rLoop;
+            status.Text = "Setup "+mspf+"ms timer";
+
+            balls = new Ball[limit]; //populate balls
+            for (int i = 0; i < balls.Length; i++)
+            {
+                balls[i] = new Ball();
+            }
+            GraphicsDrawable.bindArray(balls); //link ball array to draw function
+            status.Text = "created "+limit+" balls";
+
+            timer.Start();
+            status.Text = "loop started with params: "+limit+" balls, "+mspf+" mspf, "+compmultiplier+" multiplier";
+        }
+        public void updateValues(Object s, EventArgs e)
+        {
+            try
+            {
+                int n;
+                double m;
+                if (int.TryParse(nBalls.Text, out n))
+                {
+                    Array.Resize(ref balls, n);
+                    for (int i = 0; i < balls.Length; i++) if (balls[i] == null) balls[i] = new Ball(); //resize array, fill gaps, send new array ref to draw function
+                    GraphicsDrawable.bindArray(balls);
+                };
+                if (double.TryParse(fps.Text, out m)) frameTarget = m;
+                if (double.TryParse(spMult.Text, out m)) multiplier = m; //update timer controls for new FPS
+                mspf = (int)(1000 / frameTarget);
+                compmultiplier = multiplier * mspf * 60 / 1000;
+                timer.Interval = mspf;
+
+                status.Text = "updated params: " + limit + " balls, " + mspf + " mspf, " + compmultiplier + " multiplier";
+            } catch { status.Text = "Failed to parse some or all values"; }
+        }
+
+        void rLoop(Object s, ElapsedEventArgs e) //main loop: run update logic for all balls, render
+        {
+            foreach (var ball in balls) ball.updatePosition(canvas.Width, canvas.Height, compmultiplier);
+            canvas.Invalidate();
+        }
+    }
+    public class GraphicsDrawable : IDrawable
+    {
+        static Ball[] b;
+        static public void bindArray(Ball[] arr) { b = arr; } //array binder func
+        public void Draw(ICanvas canvas, RectF w)
+        {
+            foreach (var ball in b) //render balls
+            {
+                canvas.FillColor = ball.c;
+                canvas.FillCircle(new Point(ball.x, ball.y), (double)ball.s);
+            }
+        }
+    }
+    public class Ball
+    {
+        public double x, y, dx, dy;
+        public int s;
+        public Color c;
+        static Random r = new Random();
+        public Ball(double x, double y, double dx, double dy, int s, Color c) //main constructor
+        {
+            this.x = x;
+            this.y = y;
+            this.dx = dx;
+            this.dy = dy;
+            this.s = s;
+            this.c = c;
+        }
+        public Ball() : this(5, 5, r.NextDouble()*10+5, r.NextDouble() * 10 + 5, r.Next(5, 15), Color.FromHsv(r.Next(0, 360), 255, 255)) { } //overload redirect to make random balls
+
+        public void updatePosition(double maxX, double maxY, double mult)
+        {
+            if (x - s < 0 && dx < 0 || x + s > maxX && dx > 0) { dx *= -1; dy += r.NextDouble() - 0.5; } //check X collisions. On collide also adjust dY a bit for extra random
+            if (y - s < 0 && dy < 0 || y + s > maxY && dy > 0) { dy *= -1; dx += r.NextDouble() - 0.5; } //same on other axis
+            x += dx * mult; //move
+            y += dy * mult;
+        }
+    }
+}
